613ca0f72675302533055626fbb72a2458c0fb54
Mikhail <misha.evkarpiev@mail.ru>
2023-02-27 17:56:36 +0300

    Added main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+class LRUCache:
+    def __init__(self, capacity: int = 10) -> None:
+        pass
+
+    def get(self, key: str) -> str:
+        pass
+
+    def set(self, key: str, value: str) -> None:
+        pass
+
+    def rem(self, key: str) -> None:
+        pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
