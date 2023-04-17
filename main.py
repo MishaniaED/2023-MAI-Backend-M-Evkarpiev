295e146f23f03fbe5eb7e014969bc9b4d9b01f6d
Mikhail <misha.evkarpiev@mail.ru>
2023-04-17 19:32:20 +0300

    All done

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+from collections import OrderedDict
+# from cache import LRUCache
+
+
 class LRUCache:
     def __init__(self, capacity: int = 10) -> None:
-        pass
+        self.cache = OrderedDict()
+        self.capacity = capacity
 
     def get(self, key: str) -> str:
-        pass
+        if key not in self.cache:
+            return ''
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
 
     def set(self, key: str, value: str) -> None:
-        pass
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
 
     def rem(self, key: str) -> None:
-        pass
+        if key in self.cache:
+            self.cache.pop(key)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    cache = LRUCache(3)
+    cache.set('Jesse', 'Pinkman')
+    print(cache.cache)
+    cache.set('Walter', 'White')
+    print(cache.cache)
+    cache.set('Jesse', 'James')
+    print(cache.cache)
+    cache.set('Jessee', 'Pinkman')
+    print(cache.cache)
+    print(cache.get('Jesse'))  # вернёт 'James'
+    print(cache.cache)
+    cache.rem('Walter')
+    print(cache.cache)
+    print(cache.get('Walter'))  # вернёт ''
